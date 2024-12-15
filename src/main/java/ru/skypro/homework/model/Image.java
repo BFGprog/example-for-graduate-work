ca4c50986a06deef8ddf6105403f11f98e4333f2
BFGprog <karpov0005@gmail.com>
2024-12-15 23:48:37 +0300

    Add method uploadImage

---
+++ src/main/java/ru/skypro/homework/model/Image.java
@@ -0,0 +1,58 @@
+package ru.skypro.homework.model;
+
+import javax.persistence.*;
+import java.util.Arrays;
+import java.util.Objects;
+
+@Entity
+@Table(name="image")
+public class Image {
+
+
+    @SequenceGenerator(name = "generatorImageId", allocationSize = 1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "generatorImageId")
+    private Long id;
+    private byte[] data;
+
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", data=" + Arrays.toString(data) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return Objects.equals(id, image.id) && Arrays.equals(data, image.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id);
+        result = 31 * result + Arrays.hashCode(data);
+        return result;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
--- src/main/java/ru/skypro/homework/service/impl/UsersServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/UsersServiceImpl.java
@@ -1,11 +1,21 @@
 package ru.skypro.homework.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPasswordDto;
 import ru.skypro.homework.dto.UserDto;
+import ru.skypro.homework.service.ImageService;
 import ru.skypro.homework.service.UsersService;
+
+import java.io.IOException;
+
 @Service
 public class UsersServiceImpl implements UsersService {
+    ImageService imageService;
+
+    public UsersServiceImpl(ImageService imageService) {
+        this.imageService = imageService;
+    }
 
     @Override
     public String setPassword(NewPasswordDto newPasswordDto) {
@@ -22,5 +32,9 @@ public class UsersServiceImpl implements UsersService {
         return null;
     }
 
-
+    @Override
+    public String uploadImage(MultipartFile file) throws IOException {
+        imageService.uploadImage(file);
+        return null;
+    }
 }
